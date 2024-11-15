1c3c0f3c8bd7bd943cc604802835c0e625d8db21
Glenn Sills <gbsills@proton.me>
2024-11-15 14:49:14 -0500

    Push to origin

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	// Define a command line flag
+	maxvalue := flag.Int("maxvalue", 15, "the maximum integeter to test")
+
+	// Parse the command line flags
+	flag.Parse()
+
+	// Use the flag value
+	fmt.Println("The maximum integer to test is", *maxvalue)
+
+	for i := 1; i <= *maxvalue; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("FizzBuzz")
+
+		case i%3 == 0:
+			fmt.Println("Fizz")
+
+		case i%5 == 0:
+			fmt.Println("Buzz")
+
+		default:
+			fmt.Println(i)
+
+		}
+	}
+}
